76237041fb6dd06d139f8eb7be2fbcabb39d302c
Igor Dimitrijevic <igor@backbase.com>
2014-12-24 12:54:26 +0100

    fix merge mess

--- index.js
+++ index.js
@@ -222,19 +222,11 @@ var JXON = new (function () {
     }
   };
 
-<<<<<<< HEAD
   if (typeof window.DOMParser !== "undefined") {
     this.stringToXml = function(xmlStr) {
         return ( new window.DOMParser() ).parseFromString(xmlStr, 'application/xml');
     };
   } else if (typeof window.ActiveXObject !== "undefined" && new window.ActiveXObject("Microsoft.XMLDOM")) {
-=======
-  if (typeof window.DOMParser != "undefined") {
-    this.stringToXml = function(xmlStr) {
-        return ( new window.DOMParser() ).parseFromString(xmlStr, 'application/xml');
-    };
-  } else if (typeof window.ActiveXObject != "undefined" && new window.ActiveXObject("Microsoft.XMLDOM")) {
->>>>>>> 9b97b72b97029102226c1d027202b02de8583442
     this.stringToXml = function(xmlStr) {
         var xmlDoc = new window.ActiveXObject("Microsoft.XMLDOM");
         xmlDoc.async = "false";

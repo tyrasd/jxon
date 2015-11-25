dad529397eefb848d655dc78af8dacff9f472530
Martin Raifer <martin@bikecitizens.net>
2015-11-25 18:12:07 +0100

    typo in inline documentation

--- index.js
+++ index.js
@@ -172,7 +172,7 @@
           } else if (sName === sAttrProp) { /* verbosity level is 3 */
             for (var sAttrib in vValue) { oParentEl.setAttribute(sAttrib, vValue[sAttrib]); }
           } else if (sName === sAttrsPref+'xmlns') {
-            // do nothing: spacial handling of xml namespaces is done via createElementNS()
+            // do nothing: special handling of xml namespaces is done via createElementNS()
           } else if (sName.charAt(0) === sAttrsPref) {
             oParentEl.setAttribute(sName.slice(1), vValue);
           } else if (vValue.constructor === Array) {

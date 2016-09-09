a7ac3b3ecb1a0a11fbd36489ac21859009c14df3
Victor De Blas <vdeblas@werfen.local>
2016-09-09 13:04:13 +0200

    Solved a problem for undefined values

--- index.js
+++ index.js
@@ -172,7 +172,7 @@
 
         for (var sName in oParentObj) {
           vValue = oParentObj[sName];
-          if (vValue === null) vValue = {};
+          if (vValue === undefined || vValue === null) vValue = {};
           if (isFinite(sName) || vValue instanceof Function) { continue; } /* verbosity level is 0 */
           // when it is _
           if (sName === sValProp) {
